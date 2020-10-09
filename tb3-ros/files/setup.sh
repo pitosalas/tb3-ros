0298f098c213877d4788cdc7778100a64304d1f3
Julian Ho <julianho@brandeis.edu>
2020-10-09 19:04:44 +0800

    Remove color prompt

--- tb3-ros/files/setup.sh
+++ tb3-ros/files/setup.sh
@@ -16,6 +16,3 @@ alias gs='git status'
 alias cw='cd $NAME_CATKIN_WORKSPACE'
 alias cs='cd $NAME_CATKIN_WORKSPACE/src'
 alias cm='cd $NAME_CATKIN_WORKSPACE && catkin_make'
-
-# Enable color prompt
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
