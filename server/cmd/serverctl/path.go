334484e62740c3cebcf1e7be1da5bb2add7bf599
Julian Ho <sscj2010@gmail.com>
2021-01-03 21:20:40 +0800

    Move server component to campusrover/clouddesktop-compose

--- server/cmd/serverctl/path.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"path/filepath"
-)
-
-func buildPath(buildDir string, file string) string {
-	return filepath.Join(buildDir, file)
-}
--- server/internal/server/const.go
+++
@@ -1,8 +0,0 @@
-package server
-
-const (
-	// ComposeFileName is the name of the generated docker-compose file.
-	ComposeFileName = "docker-compose.yaml"
-	// OverviewFileName is the name of the generated overview file.
-	OverviewFileName = "overview.md"
-)
